7a997f549e05a3d09b823e8c4a2511b755bd568d
Malanius Privierre <malaniusprivierre@gmail.com>
2024-03-29 19:53:37 +0100

    fix(treehouse): trim and lowercase name from input

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -4,7 +4,7 @@ use std::io::stdin;
 fn get_name() -> String {
     let mut name = String::new();
     stdin().read_line(&mut name).expect("Failed to read line");
-    name
+    name.trim().to_lowercase()
 }
 
 fn main() {
