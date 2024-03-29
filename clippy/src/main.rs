efc7ebdacdf8ad6e2f62d5c6ca2d795f3c685e29
Malanius Privierre <malaniusprivierre@gmail.com>
2024-03-29 18:46:51 +0100

    feat(clippy): enable pedantic mode

--- clippy/src/main.rs
+++ clippy/src/main.rs
@@ -1,3 +1,7 @@
+// Turning Clippy up to pedantic can feel like an exercise in masochism,
+// but it's a great way to improve your code.
+#![warn(clippy::all, clippy::pedantic)]
+
 fn main() {
     let my_list = ["One", "Two", "Three"];
     for item in &my_list {
