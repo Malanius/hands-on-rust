ef04376096bfbddf3a489cb66bf7ffc15debb386
Malanius Privierre <malaniusprivierre@gmail.com>
2024-03-31 12:22:56 +0200

    feat(flappy): implement basic play mode

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -7,6 +7,10 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 struct Player {
     x: i32,
     y: i32,
@@ -72,9 +76,25 @@ impl State {
         }
     }
 
-    fn play(&mut self, _ctx: &mut BTerm) {
-        // TODO: Implement the game
-        self.mode = GameMode::End;
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+
+        ctx.print(1, 1, "Press SPACE to flap.");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn restart(&mut self) {
