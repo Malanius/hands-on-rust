688c51db3069a689f93a6529c7505bae8f0ca00d
Malanius Privierre <malaniusprivierre@gmail.com>
2024-04-01 10:18:41 +0200

    feat(flappy): add obstacles to the play mode

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -87,7 +87,9 @@ impl Obstacle {
 struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl State {
@@ -95,7 +97,9 @@ impl State {
         Self {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0,
         }
     }
 
@@ -128,9 +132,17 @@ impl State {
 
         self.player.render(ctx);
 
-        ctx.print(1, 1, "Press SPACE to flap.");
+        ctx.print(0, 0, "Press SPACE to flap.");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
 
-        if self.player.y > SCREEN_HEIGHT {
+        self.obstacle.render(ctx, self.player.x);
+
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
