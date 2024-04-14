a830e487c819b96afaec37c177e5b33f726fb7c7
Malanius Privierre <malaniusprivierre@gmail.com>
2024-04-14 12:18:14 +0200

    feat(dungeon): index the map

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -11,6 +11,9 @@ pub enum TileType {
 pub struct Map {
     pub tiles: Vec<TileType>,
 }
+pub fn map_idx(x: i32, y: i32) -> usize {
+    (y * SCREEN_WIDTH + x) as usize
+}
 
 impl Map {
     pub fn new() -> Self {
