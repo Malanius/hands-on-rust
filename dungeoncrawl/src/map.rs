6e7d7861298ecc86a0b1a36b39855912daa66e4c
Malanius Privierre <malaniusprivierre@gmail.com>
2024-04-14 12:13:05 +0200

    feat(dungeon): create tile type enum

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -1,3 +1,9 @@
 use crate::prelude::*;
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
