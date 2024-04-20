6ff7bc2ce2523aa5b1be13a4f2bd7eaf8aec8896
Malanius Privierre <malaniusprivierre@gmail.com>
2024-04-20 10:32:13 +0200

    feat(dungeon): use generated map in the game

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,15 +1,15 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod map;
+mod map_builder;
 mod player;
-mod map_buider;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
+    pub use crate::map_builder::*;
     pub use crate::player::*;
-    pub use crate::map_buider::*;
 }
 
 use prelude::*;
@@ -21,9 +21,11 @@ struct State {
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: Map::new(),
-            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
