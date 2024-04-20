305173f583a5973ea38d2b520377c40f2d9559be
Malanius Privierre <malaniusprivierre@gmail.com>
2024-04-20 11:00:51 +0200

    feat(dungeon): render only visible part of the map

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,4 +1,5 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod camera;
 mod map;
 mod map_builder;
 mod player;
@@ -9,6 +10,7 @@ mod prelude {
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
@@ -19,6 +21,7 @@ use prelude::*;
 struct State {
     map: Map,
     player: Player,
+    camera: Camera,
 }
 
 impl State {
@@ -28,6 +31,7 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
@@ -36,7 +40,7 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         self.player.update(ctx, &self.map);
-        self.map.render(ctx);
+        self.map.render(ctx, &self.camera);
         self.player.render(ctx);
     }
 }
